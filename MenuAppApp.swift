97b47be5b0d3f32b4185fd39a42f6e69eeacb24f
Sahil Prasad <25f1001854@ds.study.iitm.ac.in>
2025-06-30 03:18:32 +0530

    ProjectComplete

---
+++ MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Sahil Prasad on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
